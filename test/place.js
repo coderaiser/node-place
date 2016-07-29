910f02ce1fbed06400e410c9d3f46ac08c19255f
coderaiser <mnemonic.enemy@gmail.com>
2016-07-29 20:17:12 +0300

    test(place) add

---
+++ test/place.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const test = require('tape');
+const fs = require('fs');
+const place = require('..');
+const sinon = require('sinon');
+const callback = sinon.stub();
+
+fs.readFile = sinon.stub();
+fs.writeFile = sinon.stub();
+
+test('place: read', (t) => {
+    fs.readFile = sinon.stub();
+    fs.writeFile = sinon.stub();
+    
+    place('1.txt', 'a', 'b', callback);
+    
+    t.ok(fs.readFile.calledWithMatch('1.txt'), 'read should have been called');
+    t.end();
+});
+
+test('place: write', (t) => {
+    fs.readFile = (name, enc, callback) => callback(null, 'hello');
+    
+    place('1.txt', 'hello', 'world', callback);
+    
+    t.ok(fs.writeFile.calledWithMatch('1.txt', 'world'), 'callback should have been called');
+    t.end();
+});
+
+test('place: error', (t) => {
+    const fn = sinon.stub();
+    
+    fs.readFile = (name, enc, callback) => callback('error');
+    
+    place('1.txt', 'hello', 'world', fn);
+    
+    t.ok(fn.calledWith('error'), 'callback should have been called');
+    t.end();
+});
+
+test('place: arguments: no', (t) => {
+    t.throws(place, /"name" should not be empty!/, 'should throw when no name');
+    t.end();
+});
+
+test('place: arguments: "from" is not a string', (t) => {
+    const fn = () => place('1.txt');
+    
+    t.throws(fn, /"from" should be a string!/, 'should throw when "from" is not a string');
+    t.end();
+});
+
+test('place: arguments: "to" is not a string', (t) => {
+    const fn = () => place('1.txt', '');
+    
+    t.throws(fn, /"to" should be a string!/, 'should throw when "to" is not a string');
+    t.end();
+});
+
+test('place: arguments: "fn" not a function', (t) => {
+    const fn = () => place('1.txt', '', '11');
+    
+    t.throws(fn, /"callback" should be a function!/, 'should throw when "fn" not a function');
+    t.end();
+});
+
